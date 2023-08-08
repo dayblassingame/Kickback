ca589b298a58a1418691eb028307c83f0b58da3d
Day Blassingame <dayblassingame@gmail.com>
2023-08-08 16:02:23 -0400

    profile pic and logout added to nav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ export function App(){
     return(
        <main>
             <nav>
+                <a href="#profile">Profile Pic</a>
                 <a href='#home'><i id='homeIcon'></i></a>
                 <a href='#messages'><i id='messageIcon'></i></a>
                 <a href='#notifications'><i id='bellIcon'></i></a>
                 <a href='#settings'><i id='gearIcon'></i></a>
+                <a href="#logout"><i id='logout'></i></a>
             </nav>
        </main>
     );
